7ce910b72339feb52fedb562941c959655e2c829
lucaslov <czyzukasz2402@gmail.com>
2019-11-17 16:16:49 +0100

    Add CRUD actions and views for Patients

---
+++ Controllers/PatientController.cs
@@ -0,0 +1,59 @@
+﻿using ClinicManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClinicManager.Controllers
+{
+    public class PatientController : Controller
+    {
+        private ApplicationDbContext _context;
+        public PatientController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        public ActionResult Index()
+        {
+            var patients = _context.Patients.ToList();
+            return View(patients);
+        }
+        public ActionResult New()
+        {
+            return View("PatientForm");
+        }
+        public ActionResult Details(int id)
+        {
+            var patient = _context.Patients.FirstOrDefault(p => p.Id == id);
+            return View(patient);
+        }
+        public ActionResult Edit(int id)
+        {
+            var patient = _context.Patients.FirstOrDefault(p => p.Id == id);
+            if (patient == null) return HttpNotFound();
+            return View("PatientForm", patient);
+        }
+        public ActionResult Save(Patient patient)
+        {
+            if (patient.Id == 0) _context.Patients.Add(patient);
+            else
+            {
+                var patientInDb = _context.Patients.SingleOrDefault(p => p.Id == patient.Id);
+                patientInDb.FullName = patient.FullName;
+                patientInDb.BirthDate = patient.BirthDate;
+                patientInDb.PhoneNumber = patient.PhoneNumber;
+                patientInDb.EMail = patient.EMail;
+                patientInDb.Address = patient.Address;
+                patientInDb.Height = patient.Height;
+                patientInDb.Weight = patient.Weight;
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Patient");
+        }
+    }
+}
