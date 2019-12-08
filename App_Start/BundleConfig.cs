0fd6e91efcc39c33d71ac2c11296190743acc363
lucaslov <czyzukasz2402@gmail.com>
2019-12-08 19:56:11 +0100

    Add and configure jquery datetime picker in patientForm and AppointmentForm

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -5,26 +5,29 @@ namespace ClinicManager
 {
     public class BundleConfig
     {
-        // Aby uzyskać więcej informacji o grupowaniu, odwiedź stronę https://go.microsoft.com/fwlink/?LinkId=301862
-        public static void RegisterBundles(BundleCollection bundles)
+         public static void RegisterBundles(BundleCollection bundles)
         {
+            //script bundles
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
-
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
-
-            // Użyj wersji deweloperskiej biblioteki Modernizr do nauki i opracowywania rozwiązań. Następnie, kiedy wszystko będzie
-            // gotowe do produkcji, użyj narzędzia do kompilowania ze strony https://modernizr.com, aby wybrać wyłącznie potrzebne testy.
+            bundles.Add(new ScriptBundle("~/bundles/dtpickerjquery").Include(
+                       "~/Scripts/jquery-ui-1.12.1.min.js",
+                       "~/Scripts/jquery.datetimepicker.js"));
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
-
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
-
+            //style bundles
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/bundles/dtpickerstyles").Include(
+                      "~/Content/themes/base/jquery-ui.min.css",
+                      "~/Content/jquery.datetimepicker.css"));
+
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
